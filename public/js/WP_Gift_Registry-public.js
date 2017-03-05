47348222bf7eb84c77300b5a6d620fe42c964450
3qbik <3qbik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-03-05 17:37:45 +0000

    Javascript Bugfix
    git-svn-id: http://plugins.svn.wordpress.org/wpgiftregistry/trunk@1608200 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/WP_Gift_Registry-public.js
+++ public/js/WP_Gift_Registry-public.js
@@ -9,35 +9,34 @@
 		const wishlistItems = document.querySelectorAll('.wishlist li');
 		const overlay = document.querySelector('.wishlist .overlay');
 
-		overlay.querySelector('button#no').addEventListener('click', () => {
-			overlay.classList.toggle('hidden');
-		});
-
-		overlay.querySelector('button#yes').addEventListener('click', (e) => {
-
-			$.ajax({
-				url: variables.ajaxurl,
-				type: 'POST',
-				dataType: 'json',
-				data: {
-					action: 'update_gift_availability',
-					nonce: variables.updateGiftAvailabiltyNonce,
-					itemName: e.target.dataset.itemName,
-					availability: 'false'
-				},
-			})
-			.done(function(response) {
-
-				// update the button
-				document.querySelector('li[data-item-name="' + e.target.dataset.itemName + '"] .buy-button').classList.toggle('unavailable');
-
+		if ( overlay ) {
+			overlay.querySelector('button#no').addEventListener('click', () => {
+				overlay.classList.toggle('hidden');
 			});
 
-			overlay.classList.toggle('hidden');
-		});
+			overlay.querySelector('button#yes').addEventListener('click', (e) => {
 
+				$.ajax({
+					url: variables.ajaxurl,
+					type: 'POST',
+					dataType: 'json',
+					data: {
+						action: 'update_gift_availability',
+						nonce: variables.updateGiftAvailabiltyNonce,
+						itemName: e.target.dataset.itemName,
+						availability: 'false'
+					},
+				})
+				.done(function(response) {
 
+					// update the button
+					document.querySelector('li[data-item-name="' + e.target.dataset.itemName + '"] .buy-button').classList.toggle('unavailable');
 
+				});
+
+				overlay.classList.toggle('hidden');
+			});
+		}
 
 		wishlistItems.forEach(item => item.querySelector('.buy-button').addEventListener('click', (e) => {
 			//e.preventDefault();
