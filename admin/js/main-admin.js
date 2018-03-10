c4bf798be246cc87eb19d812c79afd6accfa7573
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2018-03-11 00:10:29 +0100

    merged main-admin.js

--- admin/js/main-admin.js
+++ admin/js/main-admin.js
@@ -41,6 +41,42 @@ var global = (function($) {
 
 })(jQuery);
 
+var adminJS = (function($) {
+
+    var $box = $( '#wpgr_wishlist_repeat, #wishlist_group_repeat' );
+    var replaceTitles = function() {
+        $box.find( '.cmb-group-title' ).each( function() {
+            var $this = $( this );
+            var txt = $this.next().find( '[id$="gift_title"]' ).val();
+            var rowindex;
+            if ( ! txt ) {
+                txt = $box.find( '[data-grouptitle]' ).data( 'grouptitle' );
+                if ( txt ) {
+                    rowindex = $this.parents( '[data-iterator]' ).data( 'iterator' );
+                    txt = txt.replace( '{#}', ( rowindex + 1 ) );
+                }
+            }
+            if ( txt ) {
+                $this.text( txt );
+            }
+        });
+    };
+    var replaceOnKeyUp = function( evt ) {
+        var $this = $( evt.target );
+        var id = 'title';
+        if ( evt.target.id.indexOf(id, evt.target.id.length - id.length) !== -1 ) {
+            $this.parents( '.cmb-row.cmb-repeatable-grouping' ).find( '.cmb-group-title' ).text( $this.val() );
+        }
+    };
+    $box
+        .on( 'cmb2_add_row cmb2_shift_rows_complete', function( evt ) {
+            replaceTitles();
+        })
+        .on( 'keyup', replaceOnKeyUp );
+    replaceTitles();
+
+})(jQuery);
+
 /******************************************************************
     _EXAMPLE.JS
 
@@ -87,41 +123,5 @@ var example = (function($) {
     return {
         // your code here
     };
-
-})(jQuery);
-
-var adminJS = (function($) {
-
-    var $box = $( '#wpgr_wishlist_repeat, #wishlist_group_repeat' );
-    var replaceTitles = function() {
-        $box.find( '.cmb-group-title' ).each( function() {
-            var $this = $( this );
-            var txt = $this.next().find( '[id$="gift_title"]' ).val();
-            var rowindex;
-            if ( ! txt ) {
-                txt = $box.find( '[data-grouptitle]' ).data( 'grouptitle' );
-                if ( txt ) {
-                    rowindex = $this.parents( '[data-iterator]' ).data( 'iterator' );
-                    txt = txt.replace( '{#}', ( rowindex + 1 ) );
-                }
-            }
-            if ( txt ) {
-                $this.text( txt );
-            }
-        });
-    };
-    var replaceOnKeyUp = function( evt ) {
-        var $this = $( evt.target );
-        var id = 'title';
-        if ( evt.target.id.indexOf(id, evt.target.id.length - id.length) !== -1 ) {
-            $this.parents( '.cmb-row.cmb-repeatable-grouping' ).find( '.cmb-group-title' ).text( $this.val() );
-        }
-    };
-    $box
-        .on( 'cmb2_add_row cmb2_shift_rows_complete', function( evt ) {
-            replaceTitles();
-        })
-        .on( 'keyup', replaceOnKeyUp );
-    replaceTitles();
 
 })(jQuery);
