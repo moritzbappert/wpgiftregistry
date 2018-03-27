f2ffdb92b141f2e7f95e40e075ebc1bf2d2cb495
3qbik <3qbik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-03-27 22:59:09 +0000

    TASK: frontend workflow updated
    git-svn-id: http://plugins.svn.wordpress.org/wpgiftregistry/trunk@1848067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/main.js
+++ public/js/main.js
@@ -41,6 +41,55 @@ var global = (function($) {
 
 })(jQuery);
 
+/******************************************************************
+    _EXAMPLE.JS
+
+        > VARS
+        > EVENTS
+        > FUNCTIONS
+        > PUBLIC_FUNCTIONS
+
+        @USAGE
+        e.g. nMain.showNav();
+        e.g. $(window).on('scroll', global.debounce(nMain.hideNav, 1000));
+
+******************************************************************/
+
+
+var example = (function($) {
+
+
+    /******************************************************************
+        VARS
+    ******************************************************************/
+
+    // your code here
+
+
+    /******************************************************************
+        EVENTS
+    ******************************************************************/
+
+    // your code here
+
+
+    /******************************************************************
+        FUNCTIONS
+    ******************************************************************/
+
+    // your code here
+
+
+    /******************************************************************
+        PUBLIC_FUNCTIONS
+    ******************************************************************/
+
+    return {
+        // your code here
+    };
+
+})(jQuery);
+
 var mCard = (function($) {
 
     /******************************************************************
@@ -312,51 +361,56 @@ var mPopup = (function($) {
 
 })(jQuery);
 
-/******************************************************************
-    _EXAMPLE.JS
+(function( $ ) {
+    'use strict';
 
-        > VARS
-        > EVENTS
-        > FUNCTIONS
-        > PUBLIC_FUNCTIONS
+    /**
+     * DOCUMENT READY
+     */
+    $(function() {
 
-        @USAGE
-        e.g. nMain.showNav();
-        e.g. $(window).on('scroll', global.debounce(nMain.hideNav, 1000));
+        var wishlistItems = $('.wishlist li');
+        var overlay = $('.wishlist .overlay');
 
-******************************************************************/
+        if ( overlay ) {
+            $('button#no', overlay).on('click', function() {
+                overlay.toggleClass('hidden');
+            });
 
+            $('button#yes', overlay).on('click', function(e) {
+                var itemName = $(this).data('item-name');
 
-var example = (function($) {
+                $.ajax({
+                    url: variables.ajaxurl,
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        action: 'update_gift_availability',
+                        nonce: variables.updateGiftAvailabiltyNonce,
+                        itemName: itemName,
+                        availability: 'false'
+                    },
+                })
+                .done(function(response) {
 
+                    // update the button
+                    $('li[data-item-name="' + itemName + '"] .buy-button').toggleClass('unavailable');
 
-    /******************************************************************
-        VARS
-    ******************************************************************/
+                });
 
-    // your code here
+                overlay.toggleClass('hidden');
+            });
+        }
+        wishlistItems.each(function(index, el) {
+            $('.buy-button', $(this)).on('click', function(e) {
+                //e.preventDefault();
 
+                var itemName = $('h2', $(this).parent()).html();
+                $('#item-name', overlay).html(itemName);
+                $('button#yes', overlay).data('item-name', itemName);
+                overlay.toggleClass('hidden');
+            });
+        });
+    });
 
-    /******************************************************************
-        EVENTS
-    ******************************************************************/
-
-    // your code here
-
-
-    /******************************************************************
-        FUNCTIONS
-    ******************************************************************/
-
-    // your code here
-
-
-    /******************************************************************
-        PUBLIC_FUNCTIONS
-    ******************************************************************/
-
-    return {
-        // your code here
-    };
-
-})(jQuery);
+})( jQuery );
