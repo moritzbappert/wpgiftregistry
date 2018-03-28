a48a3a3b3a414fadb041117289e09b07d0f1578a
3qbik <3qbik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-03-28 07:58:48 +0000

    Bumping version number to 1.3.0 and minor readme edits.
    git-svn-id: http://plugins.svn.wordpress.org/wpgiftregistry/trunk@1848276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpgiftregistry.php
+++ wpgiftregistry.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WPGiftRegistry
  * Plugin URI:
  * Description:       A simple way to create a linked list of wishes for your wedding, birthday or other occasion.
- * Version:           1.2.5
+ * Version:           1.3.0
  * Author:            Moritz Bappert
  * Author URI:        https://dreiqbik.de
  * License:           GPL-2.0+
